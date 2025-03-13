781b1f4ddee851139bc45352bd9b7093ee676a3b
saadcraft <39559368+saadcraft@users.noreply.github.com>
2025-03-13 03:50:44 +0100

    update return user in login

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
