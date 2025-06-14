8d42e0d85cafff2eabb73f90863914def8231f9d
saadcraft <39559368+saadcraft@users.noreply.github.com>
2025-06-14 23:05:23 +0100

    feat: add Dynamic, Fqa, and HeroPictures schemas for data modeling

---
+++ src/schemas/dynamic.shema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class Dynamic extends Document {
+    @Prop({ required: true })
+    magasine: string;
+
+    @Prop({ required: true })
+    phone: string;
+
+    @Prop({ required: true })
+    email: string;
+
+    @Prop({ type: [{ type: Types.ObjectId, ref: "Fqa" }] })
+    fqa: Types.ObjectId[];
+
+    @Prop({ type: [{ type: Types.ObjectId, ref: "HeroPictures" }] })
+    heroPictures: Types.ObjectId[];
+}
+
+export const DynamicSchema = SchemaFactory.createForClass(Dynamic);
+
